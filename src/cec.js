0b6f5ebccec1cc25782c0d3ebd041d8cf87cdbfc
zklosko <31484611+zklosko@users.noreply.github.com>
2026-09-09 11:38:01 -0400

    adding buttons for input switching

--- src/cec.js
+++ src/cec.js
@@ -17,6 +17,27 @@ export const COMMANDS = {
     String(config.targetLogicalAddress),
     "--standby",
   ],
+  INPUT_1: (config) => {
+    "-d",
+    config.cecDevice,
+    "--to",
+    String(config.targetLogicalAddress),
+    "--active-source phys-addr=1.0.0.0"
+  },
+  INPUT_2: (config) => {
+    "-d",
+    config.cecDevice,
+    "--to",
+    String(config.targetLogicalAddress),
+    "--active-source phys-addr=2.0.0.0"
+  },
+  INPUT_3: (config) => {
+    "-d",
+    config.cecDevice,
+    "--to",
+    String(config.targetLogicalAddress),
+    "--active-source phys-addr=3.0.0.0"
+  }
 };
 COMMANDS.STANDBY = COMMANDS.POWER_OFF;
 
